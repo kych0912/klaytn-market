2cfb6c44f5853209679202f8008c28445858da4c
YoungCheon Kim <kyt031522@gmail.com>
2022-08-21 18:50:37 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React,{useState} from "react";
+import React,{useEffect, useState} from "react";
 import logo from './logo.svg';
 import QRCode from "qrcode.react";
-import './App.css';
+import {getBalance,fetchCardsOf} from './api/UseCaver';
 import * as KlipAPI from './api/UseKlip'; 
-import {getBalance,readCount,setCount} from './api/UseCaver';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import './market.css'
+import {Alert,Container,Card, Nav,Form,Button} from "react-bootstrap";
 
 const DEFAULT_QR_CODE = 'DEFAULT';
 const DEFAULT_ADDRESS = "0x000000000000000";
@@ -22,6 +25,8 @@ function App() {
   //UI
 
   const [qrvalue,setQrvalue] = useState(DEFAULT_QR_CODE);
+  const [tab,setTab] = useState('MINT');
+  const [mintImageUrl,setMintImageUrl] = useState("");
   //tab
   //mintinput
   //modal
@@ -31,29 +36,127 @@ function App() {
   //onClickMyCard
   //onClickMarketCard
 
-  const getUserData = () =>{
+  const fetchMarketNFTs = async () =>{
+    const _nfts = await fetchCardsOf(myAddress);
+    setNfts(_nfts);
+  }
+
+
+  const fetchMyNFTs = async () =>{
+    const _nfts = await fetchCardsOf("0x4BBF656d74e011EF2121FD6D5E8303F271Fdd53B");
+    console.log(_nfts);
+    setNfts(_nfts);
+    console.log(nfts);
+  };
+
+  const getUserData = async () =>{
     KlipAPI.getAddress(setQrvalue,async (address)=>{  
       setMyAddress(address);
       const _balance = await getBalance(address);  //비동기(async) await 
       setMyBalance(_balance);
     });
-  }
-
+  };
+  useEffect(()=>{
+    fetchMyNFTs();
+  },[])
   //getUserData
   //getBalance
-
   return (
     <div className="App">
+      <div style ={{backgroundColor : "gray", padding : 10}}>
+        <div style ={{
+          fontSize:25,
+          fontWeight:"bold"
+        }}>
+          내 지갑
+        </div>
+        <div>
+          {myAddress}
+        </div>
+        <Alert onClick = {getUserData} variant = {"balance"} 
+        style = {{
+          borderColor:"#A4B9C6",  
+          backgroundColor : "#A4B9C6",
+          fontSize :25
+          }}>
+            {myBalance}
+        </Alert>
+          {tab === "MARKET" || tab === "WALLET"? (
+            <div className="container" style ={{padding : 0, width : "100%"}}>
+              {nfts.map((nft,index) => {
+                <Card.Img className="img-responsiveS" src = {nfts[index].uri}/>
+              })}
+            </div>
+          ) :null}
+
+          {tab === "MINT" ?
+          <div className = "container" style = {{padding :0,width:"100%"}}>
+            <Card className = "text-center" style ={{color:"black", height: "50%",borderColor:"#C5B358"}}>
+              <Card.Body style = {{opacity : 0.9, backgroundColor : "black"}}>
+                {mintImageUrl !== "" ? <Card.Img src = {mintImageUrl} height = {"50%"}/> : null}
+                <Form>
+                  <Form.Group>
+                    <Form.Control
+                    value = {mintImageUrl}
+                    onChange = {(e) => {
+                      console.log(e.target.value);
+                      setMintImageUrl(e.target.value);
+                    }}
+                    type = "text"
+                    placeholder="이미지 주소를 입력해주세요"/>
+                  </Form.Group>
+                  <Button>발행하기</Button>
+                </Form>
+              </Card.Body>
+            </Card>
+          </div>:null}
+           
+      </div>
         {/*주소 잔고*/}
         {/*갤러리 */}
         {/*발행 페이지 */}
-        {/*탭 */}
+        {/*탭 */}  
         {/*모달 */}
-        <div onClick = {getUserData}>
-          잔고:{myBalance}
-          주소:{myAddress}
-        </div>
-        <QRCode value = {qrvalue}/>
+      <Container style = {{
+        backgroundColor :"white",
+        width:300,
+        height:300,
+        padding:20
+        }}>
+        <QRCode size = {256} value = {qrvalue} style = {{     
+          margin:"auto"
+          }}/>
+      </Container>
+      <button onClick = {fetchMyNFTs}>123123123123</button>
+
+
+      {/*nav바*/}
+      <nav style = {{backgroundColor :"1b1717",height:45}} className = "navbar fixed-bottom navbar-light" role = "navigation">
+        <Nav className="w-100">
+          <div classname ="d-flex flex-row- justify-content-around w-100">
+            <div onClick={() => {
+              setTab("MARKET");
+              fetchMarketNFTs();
+            }} className = "row d-flex flex-column justify-content-center align-items-center">
+              <div>MARKET</div>
+            </div>
+            
+            <div onClick={() => {
+              setTab("MINT");
+            }} className = "row d-flex flex-column justify-content-center align-items-center">
+              <div>MINT</div>
+            </div>
+
+            <div onClick={() => {
+              setTab("WALLET");
+              fetchMyNFTs();
+            }} className = "row d-flex flex-column justify-content-center align-items-center">
+              <div>WALLET</div>
+            </div>
+          </div>
+        </Nav>
+      </nav>
+      
     </div>
   );
 }
--- src/api/UseCaver.js
+++ src/api/UseCaver.js
@@ -1,6 +1,6 @@
 import Caver from 'caver-js';
-import CounterABI from '../abi/CounterABI.json';
-import {ACCESS_KEY_ID,SECRET_ACCESS_KEY,CHAIN_ID,COUNT_CONTRACT_ADDRESS } from '../Constants';
+import KIP17ABI from '../abi/KIP17TokenABI.json';
+import {ACCESS_KEY_ID,SECRET_ACCESS_KEY,CHAIN_ID,COUNT_CONTRACT_ADDRESS,NFT_CONTRACT_ADDRESS } from '../Constants';
 
 
 const option = {
@@ -14,35 +14,60 @@ const option = {
   }
   
   const caver = new Caver(new Caver.providers.HttpProvider("https://node-api.klaytnapi.com/v1/klaytn",option));  //API
-  const CountContract = new caver.contract(CounterABI,COUNT_CONTRACT_ADDRESS);
+  const NFTContract = new caver.contract(KIP17ABI,NFT_CONTRACT_ADDRESS);
 
-  export const readCount = async () =>{
-    const _count = await CountContract.methods.count().call();
-    console.log(_count);
-  };
+  
+  export const fetchCardsOf = async (address) => {
+      const _balance = await NFTContract.methods.balanceOf(address).call();
+      console.log(_balance);
+      const tokenIds =[];
+      for (let i =0;i<_balance;i++){
+        const id = await NFTContract.methods.tokenOfOwnerByIndex(address,i).call();
+        tokenIds.push(id);
+      }
+      const tokenURIs =[];
+      for (let i =0;i<_balance;i++){
+        const uri = await NFTContract.methods.tokenURI(tokenIds[i]).call();
+        tokenURIs.push(uri);
+      }
+      console.log(`${tokenIds}`);
+      console.log(`${tokenURIs}`);
+
+      const nfts = [];
+      for(let i =0;i<_balance;i++){
+        nfts.push({uri: tokenURIs[i],id: tokenIds[i]})
+      }
+      return nfts;
+  }
   
   export const getBalance = (address) =>{
     return caver.rpc.klay.getBalance(address).then((response) =>{
       const balance = caver.utils.convertFromPeb(caver.utils.hexToNumberString(response));
-      console.log(`balance : ${balance}`);
       return balance;
     }
     )
   }
+
+//  const CountContract = new caver.contract(CounterABI,COUNT_CONTRACT_ADDRESS);
+
+//   export const readCount = async () =>{
+//     const _count = await CountContract.methods.count().call();
+//     console.log(_count);
+//   };
   
-  export const setCount = async (newCount) =>{
-    try{
-      const PRIVATE_KEY = '0x01eddeb9dbabe753289a002486849a4fe45b637f7964f68f04b68df9f7121979';
-      const deployer = caver.wallet.keyring.createFromPrivateKey(PRIVATE_KEY);
-      caver.wallet.add(deployer);
+//   export const setCount = async (newCount) =>{
+//     try{
+//       const PRIVATE_KEY = '0x01eddeb9dbabe753289a002486849a4fe45b637f7964f68f04b68df9f7121979';
+//       const deployer = caver.wallet.keyring.createFromPrivateKey(PRIVATE_KEY);
+//       caver.wallet.add(deployer);
   
-      const receipt = await CountContract.methods.setCount(newCount).send({
-        from: deployer.address,
-        gas: "0x4bfd200"
-      })
-      console.log(receipt);
-    }catch(e){
-      console.log(`ERROR ${e}`);
-    }
-  } 
+//       const receipt = await CountContract.methods.setCount(newCount).send({
+//         from: deployer.address,
+//         gas: "0x4bfd200"
+//       })
+//       console.log(receipt);
+//     }catch(e){
+//       console.log(`ERROR ${e}`);
+//     }
+//   } 
   
